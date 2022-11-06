4e82ccfc360eaa4d79cb703f106717abed106bc4
Marques Webster <marques.q.webster@gmail.com>
2022-11-05 22:04:25 -0500

    api functionality with result component added

--- js/api.js
+++ js/api.js
@@ -1,116 +1,46 @@
-const apiKey = import.meta.env.VITE_API_KEY; //API Key hidden  3656e5336f1239be4dcc23527aa19e6a
-// const cID = document.getElementById('searchName'); //N00007360  N00035278
-const cName = document.getElementById('Search-btn');
-const cID = 'N00035278';
-const year = '2022'; // cycle or the time period
-const URL = `https://www.opensecrets.org/api/?method=candSummary&cid=${cID}&cycle=2022&apikey=${apiKey}&output=json`;
-//const URL = 'https://www.opensecrets.org/api/?method=candSummary&cid=N00007360&cycle=2022&apikey=3656e5336f1239be4dcc23527aa19e6a&output=json';
-const sourceURL = `https://www.opensecrets.org/members-of-congress/summary?cid=${cID}&cycle=${year}&output=json`;
-const cImageURL = `https://cdn1.opensecrets.org/congress-members/photos/${cID}.jpg`;
+const apiKey = import.meta.env.VITE_API_KEY; //API Key hidden
 
-// https://www.opensecrets.org/members-of-congress/summary?cid=N00035278&cycle=2022&output=json
-// const search_btn2 = document.getElementById('search-btn');
-// search_btn2.addEventListener('click', getData());
+// candidate IDs to test: N00007360  N00035278
+// candidate image url: https://cdn1.opensecrets.org/congress-members/photos/${cID}.jpg
 
-async function getData() {
-  const response = await fetch(URL);
-  const data = await response.json();
-  const dataFormat = data.response.summary['@attributes'];
-  const {
-    cand_name,
-    cid,
-    cycle,
-    state,
-    party,
-    chamber,
-    first_elected,
-    next_election,
-    total,
-    spent,
-    cash_on_hand,
-    debt,
-    origin,
-    source,
-    last_updated,
-  } = dataFormat;
+const cID = 'N00007360';
+const summaryURL = `https://www.opensecrets.org/api/?method=candSummary&cid=${cID}&cycle=2022&apikey=${apiKey}&output=json`;
+const contribURL = `https://www.opensecrets.org/api/?method=candContrib&cid=${cID}&cycle=2022&apikey=${apiKey}&output=json`;
 
-  // creates elements to store the data
+export async function getData() {
+  const summaryResponse = await fetch(summaryURL);
+  const summaryData = await summaryResponse.json();
+  const summaryDataFormat = summaryData.response.summary['@attributes'];
+  // summaryDataFormat output: cand_name, cid, cycle, state, party, chamber, first_elected, next_election, spent, cash_on_hand, debt, origin, source, last_updated, total
+
+  const contribResponse = await fetch(contribURL);
+  const contribData = await contribResponse.json();
+  const contribDataFormat = contribData.response.contributors.contributor;
+  // contribDataFormat output: org_name, total, pacs, indivs
+
   const resultGrid = document.getElementById('result-grid');
-  const cImage = document.createElement('img');
-  const cFName = document.createElement('p');
-  const cLName = document.createElement('p');
-  const canID = document.createElement('p');
-  const cCycle = document.createElement('p');
-  const cState = document.createElement('p');
-  const cParty = document.createElement('p');
-  const cFirst_elected = document.createElement('p');
-  const cChamber = document.createElement('p');
-  const cNext_election = document.createElement('p');
-  const cTotal = document.createElement('p');
-  const cSpent = document.createElement('p');
-  const cCash_on_hand = document.createElement('p');
-  const cDebt = document.createElement('p');
-  const cOrigin = document.createElement('p');
-  const cSource = document.createElement('p');
-  const cLast_updated = document.createElement('p');
+  const resultItem = document.createElement('div');
+  resultItem.classList.add('result-grid__item');
 
-  // assigns the values from the API to the elements created
-  cFName.textContent = `First Name: ${dataFormat.cand_name}`;
-  cLName.textContent = `Last Name: ${dataFormat.cand_name}`;
-  canID.textContent = `Canidate ID#: ${dataFormat.cid}`;
-  cImage.src = `https://cdn1.opensecrets.org/congress-members/photos/${cID}.jpg`;
-  cCycle.textContent = `Year: ${dataFormat.cycle}`;
-  cState.textContent = `State: ${dataFormat.state}`;
-  cParty.textContent = `Party: ${dataFormat.party}`;
-  cChamber.textContent = `Chamber: ${dataFormat.chamber}`;
-  cFirst_elected.textContent = `First Elected: ${dataFormat.first_elected}`;
-  cNext_election.textContent = `Next Election: ${dataFormat.next_election}`;
-  cTotal.textContent = `Total: ${dataFormat.total}`;
-  cSpent.textContent = `Spend: ${dataFormat.spent}`;
-  cCash_on_hand.textContent = `Cash on Hand: ${dataFormat.cash_on_hand}`;
-  cDebt.textContent = `Debt: ${dataFormat.debt}`;
-  cOrigin.textContent = `Origin: ${dataFormat.origin}`;
-  cSource.textContent = `Source: ${dataFormat.source}`;
-  cLast_updated.textContent = `Last Updated: ${dataFormat.last_updated}`;
+  const cImageURL = `https://cdn1.opensecrets.org/congress-members/photos/${summaryDataFormat.cid}.jpg`;
 
-  //add the values to the resultGrid
-  resultGrid.append(cFName);
-  resultGrid.append(cLName);
-  resultGrid.append(canID);
-  resultGrid.append(cImage);
-  resultGrid.append(cCycle);
-  resultGrid.append(cState);
-  resultGrid.append(cParty);
-  resultGrid.append(cChamber);
-  resultGrid.append(cFirst_elected);
-  resultGrid.append(cNext_election);
-  resultGrid.append(cTotal);
-  resultGrid.append(cSpent);
-  resultGrid.append(cCash_on_hand);
-  resultGrid.append(cDebt);
-  resultGrid.append(cOrigin);
-  resultGrid.append(cSource);
-  resultGrid.append(cLast_updated);
+  resultItem.innerHTML = `
+    <search-result
+      result-img="${cImageURL}"
+      result-name="${summaryDataFormat.cand_name}"
+      result-position="${summaryDataFormat.chamber}"
+      result-state="${summaryDataFormat.state}"
+      result-party="${summaryDataFormat.party}"
+      contrib1="${contribDataFormat[0]['@attributes'].org_name} ($${contribDataFormat[0]['@attributes'].total})"
+      contrib2="${contribDataFormat[1]['@attributes'].org_name} ($${contribDataFormat[1]['@attributes'].total})"
+      contrib3="${contribDataFormat[2]['@attributes'].org_name} ($${contribDataFormat[2]['@attributes'].total})"
+      result-link="https://www.opensecrets.org/members-of-congress/summary?cid=${summaryDataFormat.cid}"
+    ></search-result>
+  `;
 
-  // console.log(dataFormat.cid);
-  // console.log(dataFormat.cand_name);
-  // console.log(dataFormat.cash_on_hand);
-  // console.log(dataFormat.chamber);
-  // console.log(dataFormat.party);
-  // console.log(dataFormat.state);
-  // console.log(dataFormat.total);
-  // console.log(dataFormat.origin);
-  // console.log(dataFormat.first_elected);
-  // console.log(dataFormat.source);
+  resultGrid.append(resultItem);
 
-  return dataFormat;
+  console.log(contribDataFormat[0]['@attributes'].org_name);
+  console.log(contribDataFormat[1]['@attributes']);
+  console.log(contribDataFormat[2]['@attributes']);
 }
-
-getData()
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((error) => {
-    console.log('error with getData response!');
-    console.error(error);
-  });
--- js/main.js
+++ js/main.js
@@ -2,3 +2,13 @@ import '../css/style.css';
 import './components/header.js';
 import './components/button.js';
 import './components/search-result.js';
+
+import { getData } from './api.js';
+
+const candidateSearchBtn = document.getElementById('search_btn');
+
+candidateSearchBtn.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  getData();
+});
