530f840724a7ce0b0c337e6ccab46f068adfb38b
CMuller59 <103225707+CMuller59@users.noreply.github.com>
2022-11-10 19:22:59 -0500

    Create addressForm.js

---
+++ css/components/addressForm.js
@@ -0,0 +1,33 @@
+form {
+    width: 90vw;
+    max-width: 20em;
+  }
+  
+  button {
+    margin-top: 2rem;
+  }
+  
+  input {
+    border: 1px solid;
+    padding: 0.3em;
+    width: 100%;
+  }
+  
+  textarea {
+    width: 100%;
+    min-height: 6em;
+  }
+  
+  label {
+    display: block;
+    font-size: 1.2rem;
+    margin-top: 1rem;
+  }
+  
+  select {
+    font-size: 16px;
+  }
+  
+  .postal-code {
+    width: 50%;
+  }
