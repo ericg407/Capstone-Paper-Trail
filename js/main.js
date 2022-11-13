e25188ad68c43b8335e2e57dc5eb5460bb4cace2
Marques Webster <marques.q.webster@gmail.com>
2022-11-13 17:43:27 -0600

    minor changes

--- js/main.js
+++ js/main.js
@@ -3,17 +3,20 @@ import './components/header.js';
 import './components/button.js';
 import './components/search-result.js';
 
-import { getData } from './components/getData.js';
-import { searchCandName } from './components/getLegislators.js';
+import { getData } from './utility/getData.js';
+import { searchCandName } from './utility/getLegislators.js';
 
 const candidateSearchBtn = document.getElementById('hero-search_btn');
 
 candidateSearchBtn.addEventListener('click', (evt) => {
   evt.preventDefault();
 
-  var searchNameInput = document.getElementById('hero-search__name').value;
+  var searchNameInput = document.getElementById('hero-search__name');
 
-  var cid = searchCandName(searchNameInput);
+  var cid = searchCandName(searchNameInput.value);
 
   getData(cid);
+
+  searchNameInput.value = '';
+  searchNameInput.focus();
 });
